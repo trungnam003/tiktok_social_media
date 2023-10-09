302963fb59df271c029bcfb90f0f2b5e4492673b
pytuna <zikzikzikzik16@gmail.com>
2023-10-09 13:47:03 +0700

    Add missing file

---
+++ src/Tiktok.API.Infrastructure/Migrations/20230925164300_Init_Database_Identity_Video_Audio.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tiktok.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_Database_Identity_Video_Audio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Tiktok");
+
+            migrationBuilder.EnsureSchema(
+                name: "Identity");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Bio = table.Column<string>(type: "text", nullable: true),
+                    ImageUrl = table.Column<string>(type: "varchar(1024)", nullable: true),
+                    FollowerCount = table.Column<long>(type: "bigint", nullable: false, defaultValue: 0L),
+                    FollowingCount = table.Column<long>(type: "bigint", nullable: false, defaultValue: 0L),
+                    UserName = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    NormalizedUserName = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true),
+                    Email = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    NormalizedEmail = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleClaims",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    RoleId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoleClaims_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "Identity",
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserClaims",
+                schema: "Identity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserClaims_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserFollowers",
+                schema: "Tiktok",
+                columns: table => new
+                {
+                    FollowerId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    FollowingId = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFollowers", x => new { x.FollowerId, x.FollowingId });
+                    table.ForeignKey(
+                        name: "FK_UserFollowers_Users_FollowerId",
+                        column: x => x.FollowerId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserFollowers_Users_FollowingId",
+                        column: x => x.FollowingId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLogins",
+                schema: "Identity",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProviderKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLogins", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_UserLogins_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                schema: "Identity",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    RoleId = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalSchema: "Identity",
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTokens",
+                schema: "Identity",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTokens", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_UserTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Audios",
+                schema: "Tiktok",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Url = table.Column<string>(type: "varchar(255)", nullable: true),
+                    TotalUsed = table.Column<long>(type: "bigint", nullable: false),
+                    MsDuration = table.Column<long>(type: "bigint", nullable: false),
+                    SourceId = table.Column<string>(type: "varchar(50)", nullable: true),
+                    OwnerId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Audios_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Videos",
+                schema: "Tiktok",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(1024)", nullable: true),
+                    Url = table.Column<string>(type: "varchar(512)", nullable: true),
+                    TotalLove = table.Column<long>(type: "bigint", nullable: false),
+                    OwnerId = table.Column<string>(type: "varchar(50)", nullable: false),
+                    ExternalAudioId = table.Column<string>(type: "varchar(50)", nullable: true),
+                    MsDuration = table.Column<long>(type: "bigint", nullable: false),
+                    ThumbnailUrl = table.Column<string>(type: "varchar(512)", nullable: true),
+                    Tags = table.Column<string>(type: "varchar(512)", nullable: true),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Videos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Videos_Audios_ExternalAudioId",
+                        column: x => x.ExternalAudioId,
+                        principalSchema: "Tiktok",
+                        principalTable: "Audios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_Videos_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalSchema: "Identity",
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Audios_OwnerId",
+                schema: "Tiktok",
+                table: "Audios",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Audios_SourceId",
+                schema: "Tiktok",
+                table: "Audios",
+                column: "SourceId",
+                unique: true,
+                filter: "[SourceId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleClaims_RoleId",
+                schema: "Identity",
+                table: "RoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                schema: "Identity",
+                table: "Roles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserClaims_UserId",
+                schema: "Identity",
+                table: "UserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFollowers_FollowingId",
+                schema: "Tiktok",
+                table: "UserFollowers",
+                column: "FollowingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RoleId",
+                schema: "Identity",
+                table: "UserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                schema: "Identity",
+                table: "Users",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                schema: "Identity",
+                table: "Users",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_ExternalAudioId",
+                schema: "Tiktok",
+                table: "Videos",
+                column: "ExternalAudioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Videos_OwnerId",
+                schema: "Tiktok",
+                table: "Videos",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Audios_Videos_SourceId",
+                schema: "Tiktok",
+                table: "Audios",
+                column: "SourceId",
+                principalSchema: "Tiktok",
+                principalTable: "Videos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Audios_Users_OwnerId",
+                schema: "Tiktok",
+                table: "Audios");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Videos_Users_OwnerId",
+                schema: "Tiktok",
+                table: "Videos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Audios_Videos_SourceId",
+                schema: "Tiktok",
+                table: "Audios");
+
+            migrationBuilder.DropTable(
+                name: "RoleClaims",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "UserClaims",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "UserFollowers",
+                schema: "Tiktok");
+
+            migrationBuilder.DropTable(
+                name: "UserLogins",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "UserTokens",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "Roles",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "Identity");
+
+            migrationBuilder.DropTable(
+                name: "Videos",
+                schema: "Tiktok");
+
+            migrationBuilder.DropTable(
+                name: "Audios",
+                schema: "Tiktok");
+        }
+    }
+}
